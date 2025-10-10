2ff672b16d89b48cdb16b64725765da5351d50b3
Gabriel Obregon <gab.obregon89@gmail.com>
2025-10-10 16:09:26 -0300

    Se agrega un scenario mas para validar la carga de un cliente

--- cypress/journeys/features/api/CustomerApi.feature
+++ cypress/journeys/features/api/CustomerApi.feature
@@ -4,3 +4,8 @@ Feature: API - Customer Login
         Given El usuario realiza una peticion GET al endpoint correspondiente
         Then El usuario deberia recibir un codigo de estado 200
         And El usuario deberia ver en la respuesta un arreglo de clientes
+
+    Scenario: Cargando un cliente en particular
+        Given El usuario realiza una peticion GET para obtener datos de un cliente en particular
+        Then El usuario deberia recibir un codigo de estado OK 200
+        And El usuario deberia ver en la respuesta un objeto cliente con sus datos
--- cypress/journeys/step_definitions/api/CustomerApi.js
+++ cypress/journeys/step_definitions/api/CustomerApi.js
@@ -3,6 +3,7 @@ import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
 const envi = Cypress.env('ENV');
 // Obtenemos la URL base de la aplicación según el entorno
 const endpoint = Cypress.env(`${envi}`).customerUrl;
+const account = Cypress.env(`${envi}`).accountUrl;
 
 Given('El usuario realiza una peticion GET al endpoint correspondiente', () => {
     cy.request('GET', endpoint).as('getCustomersResponse');
@@ -20,3 +21,20 @@ Then('El usuario deberia ver en la respuesta un arreglo de clientes', () => {
     });
 });
 
+
+//Scenario 2: Cargando un cliente en particular
+
+Given('El usuario realiza una peticion GET para obtener datos de un cliente en particular', () => {
+    cy.request('GET', account).as('getAccountResponse');
+});
+
+Then('El usuario deberia recibir un codigo de estado OK {int}', (statusCode) => {
+    cy.get('@getAccountResponse').its('status').should('eq', statusCode);
+});
+
+Then('El usuario deberia ver en la respuesta un objeto cliente con sus datos', () => {
+    cy.get('@getAccountResponse').then((response) => {
+        expect(response.body).to.be.an('string');
+        expect(response.body.length).to.be.greaterThan(0);
+    });
+});
