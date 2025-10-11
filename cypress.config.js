0e24b83ecbfedce65e243680ff46dd802eefe63d
Gabriel Obregon <gab.obregon89@gmail.com>
2025-10-11 10:58:04 -0300

    Se agrega configuracion para reportes usando libreria mochawesome

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,10 @@
 const { defineConfig } = require("cypress");
 const createEsbuildPlugin = require("@badeball/cypress-cucumber-preprocessor/esbuild").createEsbuildPlugin;
 const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
+
+var currentDate = new Date();
+var formattedDate = ("0" + currentDate.getDate()).slice(-2) + "-" + ("0" + (currentDate.getMonth() + 1)).slice(-2) + "-" + currentDate.getFullYear();
+
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
@@ -11,5 +15,14 @@ module.exports = defineConfig({
       return config;
     },
     specPattern: "cypress/journeys/features/*/*.feature",
+  },
+  reporter: 'mochawesome',
+  reporterOptions: {
+    reportDir: 'cypress/reports', 
+    overwrite: true,
+    html: true,
+    json: false,
+    reportFilename: 'report-day-' + formattedDate,
+    reportpageTitle: 'Reporte de Automatización'
   }
 });
